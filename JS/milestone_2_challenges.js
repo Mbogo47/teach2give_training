f4c0cdaf2beb63eaadf9b4f9600eac9c208d6d2b
Mbogo47 <njokiivy47@gmail.com>
2025-05-22 09:14:10 +0300

    Completed Challenge 2

--- JS/milestone_2_challenges.js
+++ JS/milestone_2_challenges.js
@@ -12,3 +12,18 @@ const sumOfPositives = (arr) => {
 
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
 
+// Challenge 2 : Find Maximum Value
+const findMax = (arr) => {
+    let max = arr[0];
+
+    for (let i = 1; i < arr.length; i++)
+    {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([3, 7, 2, 9, 5]));
+
