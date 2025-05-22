28810039811b0f0201cafd97c061f898c32e77b9
Mbogo47 <njokiivy47@gmail.com>
2025-05-22 09:28:38 +0300

    Completed Challenge 3: Election Winner

--- JS/milestone_2_challenges.js
+++ JS/milestone_2_challenges.js
@@ -1,29 +1,52 @@
 // Challenge 1: Sum of positives
 
 const sumOfPositives = (arr) => {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] > 0) {
-            sum += arr[i]
-        }
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
     }
-    return sum;
-} 
+  }
+  return sum;
+};
 
 console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
 
 // Challenge 2 : Find Maximum Value
 const findMax = (arr) => {
-    let max = arr[0];
+  let max = arr[0];
 
-    for (let i = 1; i < arr.length; i++)
-    {
-        if (arr[i] > max) {
-            max = arr[i];
-        }
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
     }
-    return max;
-}
+  }
+  return max;
+};
 
 console.log(findMax([3, 7, 2, 9, 5]));
 
+// Challenge 3 : Election Winner
+
+const findWinner = (arr) => {
+  let winner = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i].votes > winner.votes) {
+      winner = arr[i];
+    }
+  }
+
+  return winner;
+};
+
+const candidates = [
+  { name: "Alice", votes: 50 },
+  { name: "Bob", votes: 75 },
+  { name: "Charlie", votes: 65 },
+];
+
+console.log(findWinner(candidates));
+
+
+// 
