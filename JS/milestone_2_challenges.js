ebc6a5dde5eeacb93d33fe6ff006dee785fed2f7
Mbogo47 <njokiivy47@gmail.com>
2025-05-23 15:34:16 +0300

    Challenge 9:  Count Truthy

--- JS/milestone_2_challenges.js
+++ JS/milestone_2_challenges.js
@@ -124,3 +124,18 @@ const differenceEvenOdd = (arr) => {
 }
 
 console.log(differenceEvenOdd([1, 2, 3, 4, 5, 6]));
+
+
+// Challenge 9:  Count Truthy
+function countTruthy(obj) {
+  let count = 0;
+  for (let key in obj) {
+    if (obj[key]) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countTruthy({ a: 0, b: "hello", c: false, d: 42, e: null }));
+// returns: 2  // "hello" and 42 are truthy
