6ca9d2f45b117f081fff51281eadb7f268c020c0
Mbogo47 <njokiivy47@gmail.com>
2025-05-25 15:59:46 +0300

    implemented JS functionality
    Added scores tracking using an if statement and writing it in the scoreboard
    Set an initial state in wher paper will be the deafult choice and currently draw as the dedault statement
    Created a function called getResult which checks the hands drawn by both the computer and player and compares them to get the winner of each round
    Generated a random number to set the computer choice using indexing

---
+++ rock-paper-scissors/main.js
@@ -0,0 +1,66 @@
+const choices = document.querySelectorAll(".choice-item");
+const handSections = document.querySelectorAll(".hand-section");
+const winContainer = document.querySelector(".win-container");
+const scoreBoard = document.querySelector(".header p:last-child");
+
+let playerScore = 0;
+let computerScore = 0;
+
+// Initial state before any play
+handSections[0].textContent = "✋"; // Computer
+handSections[1].textContent = "✋"; // Player
+winContainer.textContent = "Currently Draw";
+scoreBoard.textContent = `Computer ${computerScore} Player ${playerScore}`;
+
+const getResult = (player, computer) => {
+  if (player === computer) return "draw";
+
+  if (
+    //   Rock covers Scissors
+    (player === "✊" && computer === "✌️") ||
+    // Paper covers Rock
+    (player === "✋" && computer === "✊") ||
+    // Scissors cuts Paper
+    (player === "✌️" && computer === "✋")
+  ) {
+    return "player";
+  } else {
+    return "computer";
+  }
+};
+
+// Loop through each choice element
+for (const choice of choices) {
+  choice.addEventListener("click", () => {
+    const playerChoice = choice.textContent;
+
+    //   Generate a random number between 0 and 2
+    const randomNumber = Math.floor(Math.random() * 2);
+
+    //   Create an array of Computer emoji options
+    const emojis = ["✊", "✋", "✌️"];
+
+    //   Set the Computer Choice tto a random option from the array
+    const computerChoice = emojis[randomNumber];
+
+    // Display hands
+    handSections[0].textContent = computerChoice;
+    handSections[1].textContent = playerChoice;
+
+    // display winner
+    const result = getResult(playerChoice, computerChoice);
+
+    if (result === "player") {
+      playerScore++;
+      winContainer.textContent = "player wins";
+    } else if (result === "computer") {
+      computerScore++;
+      winContainer.textContent = "computer wins";
+    } else {
+      winContainer.textContent = "draw";
+    }
+
+    // Update scoreboard
+    scoreBoard.textContent = `Computer ${computerScore} Player ${playerScore}`;
+  });
+}
