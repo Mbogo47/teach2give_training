749c3753564c150b67a733af2db0d137e03bf042
Mbogo47 <njokiivy47@gmail.com>
2025-06-02 12:11:20 +0300

    Completed challenge 1 : Return the sum of two numbers

---
+++ TS/milestone_1_challenges.js
@@ -0,0 +1,6 @@
+// CHALLENGE 1: Return the Sum of Two Numbers
+var addition = function (num_1, num_2) {
+    return num_1 + num_2;
+};
+console.log(addition(3, 5));
+console.log(addition(-6, 9));
---
+++ TS/milestone_1_challenges.ts
@@ -0,0 +1,9 @@
+// CHALLENGE 1: Return the Sum of Two Numbers
+const addition = (num_1: number, num_2: number): number => {
+  return num_1 + num_2;
+};
+
+console.log(addition(3, 5));
+console.log(addition(-6, 9));
+
+
