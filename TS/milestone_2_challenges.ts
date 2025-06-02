02c2b8a3c7ea8e5879c644883a0b745287a3bd1f
Mbogo47 <njokiivy47@gmail.com>
2025-06-02 13:08:56 +0300

    Completed Challenge 1: Sum of positives

---
+++ TS/milestone_2_challenges.ts
@@ -0,0 +1,15 @@
+// Challenge 1: Sum of positives
+
+const sumOfPositives = (arr: number[]):number => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+};
+
+console.log(sumOfPositives([1, -3, 5, -2, 9, -8]));
+
+
