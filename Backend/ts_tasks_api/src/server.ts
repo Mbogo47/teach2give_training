d67f66395943961fd5dba64617cbece6dd999955
Mbogo47 <njokiivy47@gmail.com>
2025-06-18 00:28:26 +0300

    Finally fixed the error

--- Backend/ts_tasks_api/src/server.ts
+++ Backend/ts_tasks_api/src/server.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import dotenv from "dotenv";
-import router from './routes/tasks.routers.ts'
+import router from './routes/tasks.routers'
 dotenv.config();
 
 const app = express();
