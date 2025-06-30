21e82a30e5a83ca84202dcbc0c839db289deb884
Mbogo47 <njokiivy47@gmail.com>
2025-06-30 16:25:32 +0300

    created the header
    Changes to be made

--- React/zaph-tours/src/App.tsx
+++ React/zaph-tours/src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import theme from './components/theme/theme' 
+import theme from './components/theme/theme'
+import Header from "./components/Header/Header"; 
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <Header />
     </ThemeProvider>
   );
 };
