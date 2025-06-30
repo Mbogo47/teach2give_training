3fcfcd8d083342837348535d500aa45613f99491
Mbogo47 <njokiivy47@gmail.com>
2025-06-30 16:06:15 +0300

    customized a theme for my project

---
+++ React/zaph-tours/src/components/theme/theme.ts
@@ -0,0 +1,37 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#5c5552",
+    },
+    secondary: {
+      main: "#d2f898",
+    },
+    background: {
+      default: "#f7f0f5",
+      paper: "#ffffff",
+    },
+    text: {
+      primary: "#433633",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+    button: {
+      textTransform: "none",
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        containedSecondary: {
+          color: "#433633",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
