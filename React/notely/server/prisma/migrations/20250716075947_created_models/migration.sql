71f38dacc6e3d647b9a7b9e4a50ef6c65f7e9b7e
Mbogo47 <njokiivy47@gmail.com>
2025-07-16 11:16:22 +0300

    chore: initialize project with Vite client and Express server
    Client:
    - Scaffolded frontend using Vite (React + TypeScript)
    - Removed default CSS
    - Installed client dependencies (e.g. react-router-dom, axios, Mui, etc.)
    
    Server:
    - Initialized package.json for backend
    - Installed server dependencies (express, cors, dotenv)
    - Set up basic Express server in server.ts

---
+++ React/notely/server/prisma/migrations/20250716075947_created_models/migration.sql
@@ -0,0 +1,39 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "emailAddress" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "avatarImage" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Note" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "synopsis" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isDeleted" BOOLEAN NOT NULL DEFAULT false,
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+    "authorId" TEXT NOT NULL,
+
+    CONSTRAINT "Note_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_emailAddress_key" ON "User"("emailAddress");
+
+-- AddForeignKey
+ALTER TABLE "Note" ADD CONSTRAINT "Note_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ React/notely/server/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+
+const app: Express = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+  console.log(`server is listening on port ${PORT}`);
+});
